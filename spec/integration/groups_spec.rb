6f9317bfe60c8d04b7bf6eb1b4d15252b9b3afb3
Avdi Grimm <avdi@avdi.org>
2012-04-03 19:05:11 -0400

    Tag autocompletion with Chosen.

--- spec/integration/groups_spec.rb
+++ spec/integration/groups_spec.rb
@@ -16,6 +16,13 @@ feature "Viewing groups" do
 end
 
 feature "Adding a new group" do
+  background do
+    Factory(:group, :name => "Group1", :tags => ['ruby', 'rails'])
+    Factory(:group, :name => "Group2", :tags => ['javascript', 'nodejs'])
+    # Duplicate tag names
+    Factory(:group, :name => "Group3", :tags => ['javascript', 'ruby'])
+  end
+
   scenario "added successfully" do
     visit root_path
     click_link "Add New Group"
@@ -24,7 +31,8 @@ feature "Adding a new group" do
       fill_in 'Name', :with => 'ShareVision'
       fill_in 'Url', :with => 'http://sharevisionteam.org'
       fill_in 'Email', :with => 'info@sharevisionteam.org'
-      fill_in 'Tags', :with => 'ruby, mongodb'
+      select 'ruby', :from => 'Tags'
+      select 'rails', :from => 'Tags'
       attach_file 'Avatar', File.join(Rails.root, 'app/assets/images/fallback_group.png')
 
       click_on "Create Group"
@@ -34,26 +42,7 @@ feature "Adding a new group" do
     page.has_content?('ShareVision')
 
     latest_group.should have_tag('ruby')
-    latest_group.should have_tag('mongodb')
-  end
-
-  scenario "autocompleting tags" do
-    Factory(:group, :name => "Group1", :tags => ['ruby', 'rails'])
-    Factory(:group, :name => "Group2", :tags => ['javascript', 'nodejs'])
-    # Duplicate tag names
-    Factory(:group, :name => "Group3", :tags => ['javascript', 'ruby'])
-
-    visit root_path
-    click_link "Add New Group"
-
-    expected_tags = %w[ruby rails javascript nodejs].sort
-
-    completable_tags.should eq(expected_tags)
-  end
-
-  def completable_tags
-    tag_field = find_field('group_tag_list')
-    JSON.parse(tag_field['data-tags']).sort
+    latest_group.should have_tag('rails')
   end
 
   def latest_group
